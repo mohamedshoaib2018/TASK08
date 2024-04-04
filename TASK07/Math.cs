16a8e7afe32d44f3efacc50672c19342cd4bae99
Eng Mohamed Abdelaziz Shoaib <mohamedshoap.2017@gmail.com>
2024-04-04 20:30:43 +0200

    Add project files.

---
+++ TASK07/Math.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TASK08
+{
+    internal static class Math_s
+    {
+        public static double Power2 (double num) 
+        {
+            double result = num * num;
+            return result; 
+        }
+
+         public static double PowerMultible(double num,int powerOf)
+         {
+            double result = 1;
+            
+            for (int i = 1; i <= powerOf; i++)
+            {
+                result = result * num;
+            }
+            return result;
+        }
+        public static double SquareRoot(double num)
+        {
+            if (num < 0)
+            {
+                throw new ArgumentException("Cannot calculate square root of a negative number");
+            }
+
+            if (num == 0)
+            {
+                return 0;
+            }
+
+            double x = num;
+            double epsilon = 0.000001; 
+
+            while (true)
+            {
+                double nextX = 0.5 * (x + num / x); // Newton-Raphson method
+                if (Math.Abs(nextX - x) < epsilon)
+                {
+                    return nextX;
+                }
+                x = nextX;
+            }
+        }
+
+
+        public static double Max(double num1, double num2)
+        {
+            double result;
+            if (num1 > num2) { result = num1; }
+            else { result = num2; }
+            return result;
+        }
+
+        public static double Min(double num1, double num2)
+        {
+            double result;
+            if (num1 < num2) { result = num1; }
+            else { result = num2; }
+            return result;
+        }
+
+        public static decimal Absolute(decimal num1)
+        {
+            decimal result = 0;
+            if (num1 >= 0) { result = num1; }
+            else if (num1 < 0) { result = num1 * -1; }
+            
+            return result;
+        }
+
+
+    }
+    
+}
---
+++ TASK07/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TASK08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Example for Power 2 static Method");
+            Console.WriteLine("Enter a number");
+            string num1 = Console.ReadLine();
+            Console.WriteLine($"The value of the Method is {Math_s.Power2(double.Parse(num1))}");
+            Console.WriteLine("\n\n");
+            Console.WriteLine("Example for Multible Power static Method");
+            Console.WriteLine("Enter the number value");
+            string num2 = Console.ReadLine();
+            Console.WriteLine("Enter the power value");
+            string num3 = Console.ReadLine();
+            Console.WriteLine($"The value of the Method is {Math_s.PowerMultible(int.Parse(num2), int.Parse(num3))}");
+            Console.WriteLine("\n\n");
+            Console.WriteLine("Example for squareRoot static Method");
+            Console.WriteLine("Enter a number");
+            double num4 = double.Parse(Console.ReadLine());
+            Console.WriteLine($"The value of the Method is {Math_s.SquareRoot(num4)}");
+            Console.WriteLine("\n\n");
+            Console.WriteLine("Example for Max Number static Method");
+            Console.WriteLine("Enter the first number");
+            double num5 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            double num6 = double.Parse(Console.ReadLine());
+            Console.WriteLine($"The value of the Method is {Math_s.Max(num5, num6)}");
+            Console.WriteLine("\n\n");
+            Console.WriteLine("Example for Min Number static Method");
+            Console.WriteLine("Enter the first number");
+            double num7 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            double num8 = double.Parse(Console.ReadLine());
+            Console.WriteLine($"The value of the Method is {Math_s.Min(num7, num8)}");
+            Console.WriteLine("\n\n");
+            Console.WriteLine("Example for Absolute static Method");
+            Console.WriteLine("Enter a number with negative sign");
+            int num9 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"The value of the Method is {Math_s.Absolute(num9)}");
+        }
+        
+    }
+}
